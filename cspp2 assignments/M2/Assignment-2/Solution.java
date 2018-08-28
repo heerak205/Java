a8790528f6c753deb0c6b0cb435c9bc9e984b3fc
heerak205 <heerak.praffull@msitprogram.net>
2018-08-28 17:55:08 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 assignments/M2/Assignment-2/Solution.java
+++ cspp2 assignments/M2/Assignment-2/Solution.java
@@ -7,43 +7,44 @@ import java.util.Scanner;
  */
 public final class Solution {
 
-	/**
-	 * COnstant Four is 4
-	 */
-	public static final int FOUR = 4;
+    /**
+     * COnstant Four is 4
+     */
+    public static final int FOUR = 4;
 
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
+    }
 
-	/**
-	 * { function_description }.
-	 *
-	 * @param      a     { parameter_description }
-	 * @param      b     { parameter_description }
-	 * @param      c     { parameter_description }
-	 */
-	static void rootsOfEquation(final int a, final int b, final int c) {
-		double root1 = (-b + Math.sqrt((b * b - FOUR * a * c))) / (2 * a);
-		double root2 = (-b - Math.sqrt((b * b - FOUR * a * c))) / (2 * a);
-		System.out.println(root1 + " " + root2);
-	}
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int a = scan.nextInt();
-		int b = scan.nextInt();
-		int c = scan.nextInt();
-		rootsOfEquation(a, b, c);
-	}
-	/*
-	Need to write the rootsOfQuadraticEquation func
-	*/
+    /**
+     * { function_description }.
+     *
+     * @param      a     { parameter_description }
+     * @param      b     { parameter_description }
+     * @param      c     { parameter_description }
+     */
+    static void rootsOfEquation(final int a, final int b, final int c) {
+        double value = Math.sqrt((b * b - FOUR * a * c));
+        double root1 = (-b + value ) / (2 * a);
+        double root2 = (-b - value ) / (2 * a);
+        System.out.println(root1 + " " + root2);
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        rootsOfEquation(a, b, c);
+    }
+    /*
+    Need to write the rootsOfQuadraticEquation func
+    */
 }
