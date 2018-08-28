d5769e1051f8877f3cfab5db4f9e7d6a8d8e0794
heerak205 <heerak.praffull@msitprogram.net>
2018-08-28 18:08:01 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignments/M2/Assignment-3/Solution.java
+++ cspp2 assignments/M2/Assignment-3/Solution.java
@@ -6,38 +6,38 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * { function_description }
-	 *
-	 * @param      base      The base
-	 * @param      exponent  The exponent
-	 *
-	 * @return     { description_of_the_return_value }.
-	 */
-	static long power(int base, int exponent) {
-		if(exponent == 0)
-			return 1;
-		/**
-		 * { item_description }.
-		 */
-		return base*power(base, (exponent-1));
-	}
-	/*
-	Do not modify this main function.
-	*/
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+    /**
+     * { function_description }
+     *
+     * @param      base      The base
+     * @param      exponent  The exponent
+     *
+     * @return     { description_of_the_return_value }.
+     */
+    static long power(int base, int exponent) {
+        if (exponent == 0)
+            return 1;
+        /**
+         * { item_description }.
+         */
+        return base * power(base, (exponent - 1));
+    }
+    /*
+    Do not modify this main function.
+    */
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);      
         int base = s.nextInt();
         int exponent = s.nextInt();
-        long result=power(base,exponent);
+        long result = power(base,exponent);
         System.out.println(result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
+    }
+    /*
+    Need to write the power function and print the output.
+    */
 }
