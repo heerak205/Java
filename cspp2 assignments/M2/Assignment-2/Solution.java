42c241d7f23741ebf350ecaaa40af3511b76b5e5
heerak205 <heerak.praffull@msitprogram.net>
2018-08-28 17:52:16 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 assignments/M2/Assignment-2/Solution.java
+++ cspp2 assignments/M2/Assignment-2/Solution.java
@@ -1,34 +1,44 @@
 /**
- * { item_description }
+ * { item_description }.
  */
 import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+public final class Solution {
+
+	final static int FOUR = 4;
+	
 	/**
-	 * { function_description }
+	 * Constructs the object.
+	 */
+	private Solution() {
+
+	}
+
+	/**
+	 * { function_description }.
 	 *
 	 * @param      a     { parameter_description }
 	 * @param      b     { parameter_description }
 	 * @param      c     { parameter_description }
 	 */
-	static void rootsOfQuadraticEquation(final int a, final int b, final int c) {
-		double root1 = (-b + Math.sqrt((b * b - 4 * a * c))) / (2 * a);
-		double root2 = (-b - Math.sqrt((b * b - 4 * a * c))) / (2 * a);
+	static void rootsOfEquation(final int a, final int b, final int c) {
+		double root1 = (-b + Math.sqrt((b * b - FOUR * a * c))) / (2 * a);
+		double root2 = (-b - Math.sqrt((b * b - FOUR * a * c))) / (2 * a);
 		System.out.println(root1 + " " + root2);
 	}
 	/**
-	 * { function_description }
+	 * { function_description }.
 	 *
 	 * @param      args  The arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
 		int c = scan.nextInt();
-		rootsOfQuadraticEquation(a, b, c);
+		rootsOfEquation(a, b, c);
 	}
 	/*
 	Need to write the rootsOfQuadraticEquation func
