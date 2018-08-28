80aa30082e6fc68a24b27e95df18c163ba3cbebd
heerak205 <heerak.praffull@msitprogram.net>
2018-08-28 17:43:06 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2 assignments/M2/Assignment-2/Solution.java
+++ cspp2 assignments/M2/Assignment-2/Solution.java
@@ -18,15 +18,12 @@ public class Solution {
 		double root2 = (-b - Math.sqrt((b * b - 4 * a * c))) / (2 * a);
 		System.out.println(root1 + " " + root2);
 	}
-	/*
-	Do not modify this main function.
-	*/
 	/**
 	 * { function_description }
 	 *
 	 * @param      args  The arguments
 	 */
-	public static void main(String[] args) {
+	public static final void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int a = scan.nextInt();
 		int b = scan.nextInt();
