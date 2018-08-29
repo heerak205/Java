6c6e413998c13a261126807ee36a7e1635386fbe
heerak205 <heerak.praffull@msitprogram.net>
2018-08-29 17:22:26 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignments/M3/Assignment-3/Solution.java
+++ cspp2 assignments/M3/Assignment-3/Solution.java
@@ -1,12 +1,26 @@
 
+/**
+ * { item_description }.
+ */
 import java.util.Scanner;
 /*
 	Do not modify this main function.
 	*/
+/**
+ * Class for solution.
+ */
 public class Solution {
-
+    /**
+     * { item_description }.
+     */
+    private Solution() {
+    }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
-
         Scanner s=new Scanner(System.in);      
         int n1 = s.nextInt();
         int n2 = s.nextInt();
@@ -16,8 +30,16 @@ public class Solution {
     /*
 	Need to write the gcd function and print the output.
 	*/
+    /**
+     * { function_description }.
+     *
+     * @param      n1    The n 1
+     * @param      n2    The n 2
+     *
+     * @return     { description_of_the_return_value }
+     */
     public static int gcd(int n1, int n2){
-        int r = 10000;
+        int r = 100000;
         while(r > 0){
             r = n2 % n1;
             n2 = n1;
