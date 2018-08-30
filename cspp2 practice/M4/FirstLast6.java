5cb63bc0885f8e1899838f28b7b0637cd73e6d23
heerak205 <heerak.praffull@msitprogram.net>
2018-08-30 19:47:03 +0530

    first6

---
+++ cspp2 practice/M4/FirstLast6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public final class FirstLast6{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int a[] = new int[n];
+		for(int i = 0; i < a.length; i++) {
+			a[i] = s.nextInt();
+		}
+		if(a[0] == 6 || a[a.length-1] == 6) {
+			System.out.println("true"); }
+		else {
+		System.out.println("false"); }
+		
+	}	
+}
