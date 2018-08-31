4735591e8614dae927ad747a0310df660259e88e
heerak205 <heerak.praffull@msitprogram.net>
2018-08-31 15:25:02 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2 assignments/M4/Assignment-3/Solution.java
+++ cspp2 assignments/M4/Assignment-3/Solution.java
@@ -1,29 +1,46 @@
 
 // Given an String, Write a java method that returns the decimal value for the given binary string.
 
-
+/**
+ * { imports scanner class }.
+ */
 import java.util.Scanner;
+/**
+ * Class for solution.
+ */
 public class Solution
 {/*
 	Do not modify this main function.
 	*/
-	public static void main(String[] args)
+	/**
+	 * {main function}.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args)
 	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		// sc.nextLine();
-		for(int i=0;i<n;i++){
-			String s=sc.nextLine();
-			String res=binaryToDecimal(s);//Write binaryToDecimal function
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		sc.nextLine();
+		for (int i = 0; i < n; i++){
+			String s = sc.nextLine();
+			String res = binaryToDecimal(s);//Write binaryToDecimal function
 			System.out.println(res);
 		}
 	}
-	public static String binaryToDecimal(String s){
+	/**
+	 * { function to convert binary to decimal. }.
+	 *
+	 * @param      s     { parameter_description }.
+	 *
+	 * @return     { description_of_the_return_value }.
+	 */
+	public static String binaryToDecimal(String s) {
 		// System.out.println(s);
 		int sum = 0;
 		int count = 0;
 		// int res;
-		for(int i = s.length() - 1; i>=0; i--){
+		for (int i = s.length() - 1; i>=0; i--) {
 			char p = s.charAt(i);
 			int l = Character.getNumericValue(p);
 			//System.out.print(l+"*");
