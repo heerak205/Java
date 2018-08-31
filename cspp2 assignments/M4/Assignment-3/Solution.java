6af27539cc8e72aaf969821060fe4950e946c44c
heerak205 <heerak.praffull@msitprogram.net>
2018-08-31 15:27:27 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- cspp2 assignments/M4/Assignment-3/Solution.java
+++ cspp2 assignments/M4/Assignment-3/Solution.java
@@ -1,5 +1,5 @@
 
-// Given an String, Write a java method that returns the decimal value for the given binary string.
+// Given an String, Write a java method.
 
 /**
  * { imports scanner class }.
@@ -8,49 +8,48 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution
-{/*
-	Do not modify this main function.
-	*/
-	/**
-	 * {main function}.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args)
-	{
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		sc.nextLine();
-		for (int i = 0; i < n; i++){
-			String s = sc.nextLine();
-			String res = binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	/**
-	 * { function to convert binary to decimal. }.
-	 *
-	 * @param      s     { parameter_description }.
-	 *
-	 * @return     { description_of_the_return_value }.
-	 */
-	public static String binaryToDecimal(String s) {
-		// System.out.println(s);
-		int sum = 0;
-		int count = 0;
-		// int res;
-		for (int i = s.length() - 1; i>=0; i--) {
-			char p = s.charAt(i);
-			int l = Character.getNumericValue(p);
-			//System.out.print(l+"*");
-			//System.out.println(Math.pow(2, count));
-			sum = sum + (int)((Math.pow(2, count)) * l);
-			count = count + 1;
+public class Solution {
+    /*
+    Do not modify this main function.
+    */
+    /**
+     * {main function}.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s); //Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
+    /**
+     * { function to convert binary to decimal. }.
+     *
+     * @param      s     { parameter_description }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
+    public static String binaryToDecimal(final String s) {
+        // System.out.println(s);
+        int sum = 0;
+        int count = 0;
+        // int res;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char p = s.charAt(i);
+            int l = Character.getNumericValue(p);
+            //System.out.print(l+"*");
+            //System.out.println(Math.pow(2, count));
+            sum = sum + (int) ((Math.pow(2, count)) * l);
+            count = count + 1;
 
-		}
-		// res = (int) sum;
-		return String.valueOf(sum);
-	}
+        }
+        // res = (int) sum;
+        return String.valueOf(sum);
+    }
 
 }
