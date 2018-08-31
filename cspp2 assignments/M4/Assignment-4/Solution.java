1f9bc2d9ed743d7ee2d0e61da7ac869222a2e6da
heerak205 <heerak.praffull@msitprogram.net>
2018-08-31 14:05:53 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2 assignments/M4/Assignment-4/Solution.java
+++ cspp2 assignments/M4/Assignment-4/Solution.java
@@ -1,5 +1,5 @@
 
-// Given an String, Write a java method that returns the decimal value for the given binary string.
+// Given an String
 
 /**
  * { imports Scanner class}.
@@ -8,38 +8,38 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {  
-	/*
-	Do not modify the main function 
-	*/
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {		
-	}
-	/**
-	 * { main function}.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String s = sc.nextLine();
-		String reverse = reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-	//Write reverseString function
-	/**
-	a function for creating reverse of a string.
-	* @param s The arguments
-	*/
-	public static String reverseString(String s) {
-		String l = "";
-		for (int i = (s.length() - 1); i >= 0; i--) {
-			l = l + s.charAt(i);
-		}
-		return l;
-	}
+public final class Solution {
+    /*
+    Do not modify the main function 
+    */
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+    }
+    /**
+     * { main function}.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+        
+    }
+    //Write reverseString function
+    /**
+    a function for creating reverse of a string.
+    * @param s The arguments
+    */
+    public static String reverseString(final String s) {
+        String l = "";
+        for (int i = (s.length() - 1); i >= 0; i--) {
+            l = l + s.charAt(i);
+        }
+        return l;
+    }
 
 }
