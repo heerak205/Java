4d659fa1d145019b4104c030e41dcb239930f988
heerak205 <heerak.praffull@msitprogram.net>
2018-09-17 21:23:50 +0530

    "check_style_score -> 13 of 13 passed. style score: 0/1 "

--- cspp2 assignments/m20/Assignment-1/Solution.java
+++ cspp2 assignments/m20/Assignment-1/Solution.java
@@ -281,7 +281,7 @@ public final class Solution {
      * @param      scan       The scan
      * @param      quiz       The quiz
      * @param      q          The question count
-     * @throws DummyException
+     * @throws Expection @throws tag for 'Exception'Exception
      */
     public static void loadQuestions(final Scanner scan,
         final Quiz quiz, final int q) throws Exception {
